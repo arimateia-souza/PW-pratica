4f93a23855bbd7b8f5951a5583fa0f59f50d9888
arimateia-souza <arimateia.souza.123@ufrn.edu.br>
2023-04-23 14:40:21 -0300

    Atividade de cookies

--- src/main/java/atividade/web/ControllerAtividade.java
+++ src/main/java/atividade/web/ControllerAtividade.java
@@ -1,5 +1,6 @@
 package atividade.web;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -12,8 +13,6 @@ import java.io.IOException;
 
 @Controller
 public class ControllerAtividade {
-
-
     @RequestMapping(method = RequestMethod.POST,value = "/doSomething")
     public void doSomething(HttpServletResponse response, HttpServletRequest request)throws IOException{
         var writer = response.getWriter();
@@ -34,8 +33,6 @@ public class ControllerAtividade {
 
 
     }
-
-
     @RequestMapping(value = "/sessao")
         public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
             HttpSession session = request.getSession();
@@ -52,6 +49,40 @@ public class ControllerAtividade {
             var out = response.getWriter();
             out.println("id: "+id+" creationTime: " + creationTime + " lastAccessedTime: " + lastAccessedTime + " count: "+ acessoCount);
         }
+    @RequestMapping(value = "/cookie",method = RequestMethod.GET)
+        public void doCookie(HttpServletResponse response, HttpServletRequest resquest) throws IOException{
+        Cookie cookie1 = new Cookie("1", "um");
+        Cookie cookie2 = new Cookie("2", "dois");
+        Cookie cookie3 = new Cookie("3", "tres");
+        Cookie cookie4 = new Cookie("4", "quatro");
+        Cookie cookie5 = new Cookie("5", "cinco");
+
+        cookie4.setMaxAge(30); //tempo do cookie
+        cookie5.setMaxAge(30); //tempo do cookie
+
+        response.addCookie(cookie1); //adiciona o cookie na resposta
+        response.addCookie(cookie2);
+        response.addCookie(cookie3);
+        response.addCookie(cookie4);
+        response.addCookie(cookie5);
+        response.sendRedirect("/cookieVer");
+
+    }
+    @RequestMapping(value = "/cookieVer", method = RequestMethod.GET)
+        public void doCookieVer(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        var write = response.getWriter();
+        Cookie cookies[] = request.getCookies();
+        String nome, valor;
+
+        if(cookies !=null){
+            for (var c: cookies){
+                nome = c.getName();
+                valor = c.getValue();
+                write.println("Cookie: " + nome + " Valor: " + valor);
+
+            }
+        }
+    }
 
 }
 
